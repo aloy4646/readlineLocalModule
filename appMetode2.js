5f791783e2765882077eeda97fabccd6c6439d9b
Aloysius Las Roha S <aloysiuslasroha@gmail.com>
2024-03-25 10:56:46 +0700

    menambahkan pengecekan folder dan file contacts

--- appMetode2.js
+++ appMetode2.js
@@ -17,15 +17,31 @@ pertanyaan(validationConfig, 0, [], validator, (jawaban) => {
         "\nNomor handphone: " + jawaban[1] +
         "\nEmail: " + jawaban[2])
 
-    // membuat object contact berisi nama, nomorHandphone dan email
-    const contact = {nama: jawaban[0], 
-                    nomorHandphone: jawaban[1], 
-                    email: jawaban[2]}
-
-    const file = fs.readFileSync('data/contacts.json', 'utf-8')
-    const contacts = JSON.parse(file)
-    contacts.push(contact)
-    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
+    simpanContact(jawaban)
 
     console.log("Terima kasih sudah memasukkan data!")
 })
+
+function simpanContact(jawaban){
+    //membuat object contact berisi nama, nomorHandphone dan email
+    const contact = {nama: jawaban[0], 
+        nomorHandphone: jawaban[1], 
+        email: jawaban[2]}
+
+    //membuat folder data jika belum ada
+    const dirPath = './data'
+    if(!fs.existsSync(dirPath)){
+        fs.mkdirSync(dirPath)
+    }
+
+    //membuat file contacts.json jika belum ada
+    const dataPath = dirPath + "/contacts.json"
+    if(!fs.existsSync(dataPath)){
+        fs.writeFileSync(dataPath, '[]', 'utf-8')
+    }
+
+    const file = fs.readFileSync(dataPath, 'utf-8')
+    const contacts = JSON.parse(file)
+    contacts.push(contact)
+    fs.writeFileSync(dataPath, JSON.stringify(contacts))
+}
