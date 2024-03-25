6c21371c964c366257d0af4eea5e5fd98881a083
Aloysius Las Roha S <aloysiuslasroha@gmail.com>
2024-03-25 10:03:50 +0700

    menambahkan komen dan menyimpan hasil readline ke contacts.json

--- appMetode2.js
+++ appMetode2.js
@@ -1,12 +1,13 @@
 const validator = require("validator")
 const pertanyaan = require('./tanyaMetode2')
+const fs = require("fs")
 
+
+// membuat objek validationConfig yang berisi pertanyaan dan validator yang sesuai dengan pertanyaan tersebut
 const validationConfig = {
     "Siapa nama anda? ": (data, validator) => validator.isAlpha(data),
     "Berapa nomor handphone anda? ": (data, validator) => validator.isMobilePhone(data, "id-ID"),
-    // "Apa nama email anda? ": (data, validator) => validator.isEmail(data)
-    "Berapa umur anda? ": (data, validator) => validator.isNumeric(data),
-
+    "Apa nama email anda? ": (data, validator) => validator.isEmail(data)
 }
 
 pertanyaan(validationConfig, 0, [], validator, (jawaban) => {
@@ -14,6 +15,17 @@ pertanyaan(validationConfig, 0, [], validator, (jawaban) => {
         "Berikut ini adalah data diri anda\n" +
         "Nama: " + jawaban[0] +
         "\nNomor handphone: " + jawaban[1] +
-        // "\nEmail: " + jawaban[2])
-        "\nUmur: " + jawaban[2])
+        "\nEmail: " + jawaban[2])
+
+    // membuat object contact berisi nama, nomorHandphone dan email
+    const contact = {nama: jawaban[0], 
+                    nomorHandphone: jawaban[1], 
+                    email: jawaban[2]}
+
+    const file = fs.readFileSync('data/contacts.json', 'utf-8')
+    const contacts = JSON.parse(file)
+    contacts.push(contact)
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
+
+    console.log("Terima kasih sudah memasukkan data!")
 })
--- tanyaMetode2.js
+++ tanyaMetode2.js
@@ -6,16 +6,19 @@ const rl = readline.createInterface({
 })
 
 function pertanyaan(validationConfig, index, arrayJawaban, validator, callback) {
-    const pertanyaanKeys = Object.keys(validationConfig)
-    if (index >= pertanyaanKeys.length) {
+    //mengambil keys dari validationConfig (pertanyaan) menjadi sebuah array
+    const kumpulanPertanyaan = Object.keys(validationConfig)
+    if (index >= kumpulanPertanyaan.length) {
         rl.close()
         callback(arrayJawaban)
         return
     }
 
-    const currentPertanyaan = pertanyaanKeys[index]
+    //mengambil pertanyaan sesuai index
+    const currentPertanyaan = kumpulanPertanyaan[index]
 
     rl.question(currentPertanyaan, (data) => {
+        //validasi jawaban menggunakan validator yang berada di validationConfig menggunakan pertanyaan sebagai key
         const apakahValid = validationConfig[currentPertanyaan](data, validator)
         if (!apakahValid) {
             console.log("Format tidak sesuai, silahkan coba lagi\n")
